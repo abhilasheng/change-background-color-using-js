2f841a99d512a7d07335766e3ddcead47555c7e6
abhi <abhilashengala28@gmail.com>
2023-10-25 19:56:12 +0530

    unlimited random colors

--- main.js
+++ main.js
@@ -5,8 +5,15 @@ const color = ['red','yellow','brown','green','purple','blue','orange'];
 body.style.background ='black';
 
 function bgcolorchange(){
-    const colorIndex = parseInt(Math.random()*color.length-1);
-    body.style.background = color[colorIndex];
+    // const colorIndex = parseInt(Math.random()*color.length-1);
+    // body.style.background = color[colorIndex];
+    //random number generation of any size 
+    const genRanHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
+
+    indexnum = genRanHex(6);
+    body.style.background = `#${indexnum}`
+    
+
 }
 
 button.addEventListener('click',bgcolorchange);
